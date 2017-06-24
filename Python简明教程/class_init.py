efaf0c21db7dec5ced9a69fe3faccccc48596eff
badangle520 <ywy86898858@126.com>
2017-06-24 22:36:09 +0800

    p

---
+++ Python简明教程/class_init.py
@@ -0,0 +1,10 @@
+#File name = class_init.py
+
+class Person:
+    def __init__(self,name):
+	self.name = name
+    def sayHi(self):
+	print 'Hello, my name is',self.name
+
+p = Person('david')
+p.sayHi()
