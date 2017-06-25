14456d6657158f37b2e592096f9bbf1dc1ba55b3
badangle520 <ywy86898858@126.com>
2017-06-25 20:18:57 +0800

    update

---
+++ Python简明教程/using_file.py
@@ -0,0 +1,24 @@
+#File name = using_file.py
+
+poem = '''\
+test
+'''
+
+f = file('poem.txt','w')#open for 'w'riting
+f.write(poem)		#write text to file
+f.close()		#close the file	
+
+f = file('poem.txt')
+#if no mode is specified, 'r'ead mode is assumed by default
+while True:
+    line = f.readline()
+    if len(line) == 0:	#Zero length indicated EOF
+	break
+    print line,	#Notice comma to avoid automatic nreline added by python
+f.close() #close the file
+
+
+:<<!
+
+!
+
